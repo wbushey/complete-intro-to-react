c73f7d864a52d8608ef07cf1124d7b3a942cf414
Bill Bushey <wbushey@gmail.com>
2017-07-16 20:56:24 -0500

    Through State

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () =>
-  <div className="search">
-    {preload.shows.map(show => <ShowCard {...show} key={show.imdbID} />)}
-  </div>;
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: 'this is a debug statement',
+    };
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+  handleSearchTermChange(event) {
+    this.setState({searchTerm: event.target.value});
+  }
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>BestFlix</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        {preload.shows
+          .filter(
+            show =>
+              `${show.title} ${show.description}`
+                .toUpperCase()
+                .indexOf(this.state.searchTerm.toUpperCase()) >= 0,
+          )
+          .map(show => <ShowCard {...show} key={show.imdbID} />)}
+      </div>
+    );
+  }
+}
 
 export default Search;
--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,9 +1,25 @@
-import {shape, string} from 'prop-types';
+import {string} from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
+`;
 
 const ShowCard = props =>
-  <div className="show-card">
-    <img
+  <Wrapper>
+    <Image
       alt={`${props.title} show Poster`}
       src={`/public/img/posters/${props.poster}`}
     />
@@ -16,7 +32,7 @@ const ShowCard = props =>
     <p>
       {props.description}
     </p>
-  </div>;
+  </Wrapper>;
 
 ShowCard.propTypes = {
   poster: string.isRequired,
